b5ad613fb018a3163c969861e1ed6060d0c3832c
nardaiv <nardaikiru@gmail.com>
2025-01-28 15:53:34 +0700

    add navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
 
     return ( 
         <footer className="bg-primary w-full flex flex-col items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16 px-16">
-            <div className="w-full flex justify-between items-center">
+            <div className="w-4/5 flex justify-between items-center">
                 <Image
-                    src="/favicon.ico"
-                    height={100}
-                    width={100}
+                    src="/logo.png"
+                    height={150}
+                    width={150}
                     alt="Footer Logo"
                 />
                 <div className="flex w-2/5 justify-between">
@@ -24,7 +24,7 @@ export default function Footer() {
 
                     <div>
                         <p className="text-base font-bold  text-primary-foreground">Informasi Lainnya</p>
-                        <ul className="pl-4 text-left text-sm/8 text-primary-foreground group-hover:underline">
+                        <ul className="pl-4 text-left text-sm/8 text-primary-foreground ">
                             <li className="hover:underline"><a href="">Email</a></li>
                             <li className="hover:underline"><a href="">Whatsapp</a></li>
                             <li className="hover:underline"><a href="">Instagram</a></li>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,8 @@ import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
+import Image from "next/image";
+
 
 
 export default function Navbar() {
@@ -9,13 +11,30 @@ export default function Navbar() {
     return ( 
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-
+          {/* 
           <div className="flex gap-5 items-center font-semibold">
 
             <Link href={"/"}>Next.js Supabase Starter</Link>
           </div>
 
-          {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+          {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />} */}
+          <Link  href={"/"} >
+            <Image
+              src="/logo.png"
+              height={80}
+              width={80}
+              alt="Footer Logo"
+            />
+          </Link>
+
+          <div className="flex items-center gap-3">
+            <ul className="flex gap-5 text-sm group hover:[&>*]:text-primary">
+              <li><a href=""></a>Detail</li>
+              <li><a href=""></a>Testimoni</li>
+            </ul>
+
+            <HeaderAuth />
+          </div>
 
         </div>
       </nav>
