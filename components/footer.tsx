c9645b507f595ccf57621f3eec8bbcfe7a35b8d2
nardaiv <nardaikiru@gmail.com>
2025-01-28 16:03:29 +0700

    responsive footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 export default function Footer() {
 
     return ( 
-        <footer className="bg-primary w-full flex flex-col items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16 px-16">
-            <div className="w-4/5 flex justify-between items-center">
+        <footer className="bg-primary w-full flex flex-col items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16 px-8 md:px-16">
+            <div className="w-4/5 flex flex-col gap-4 sm:gap-0 sm:flex-row justify-between items-start sm:items-center">
                 <Image
                     src="/logo.png"
                     height={150}
                     width={150}
                     alt="Footer Logo"
                 />
-                <div className="flex w-2/5 justify-between">
+                <div className="flex flex-col sm:flex-row w-full sm:w-3/5 lg:w-2/5 gap-6 sm:gap-0 justify-between">
                     <div className="">
-                        <p className="text-base font-bold  text-primary-foreground">Oh My Tofu!</p>
+                        <p className="text-base font-bold  text-left text-primary-foreground">Oh My Tofu!</p>
                         <ul className="pl-4 text-left text-sm/8 text-primary-foreground group hover:[&>*]:underline">
                             <li className=""><a href="">Dashboard</a></li>
                             <li className=""><a href="">Pesan</a></li>
@@ -23,7 +23,7 @@ export default function Footer() {
                     </div>
 
                     <div>
-                        <p className="text-base font-bold  text-primary-foreground">Informasi Lainnya</p>
+                        <p className="text-base font-bold text-left  text-primary-foreground">Informasi Lainnya</p>
                         <ul className="pl-4 text-left text-sm/8 text-primary-foreground ">
                             <li className="hover:underline"><a href="">Email</a></li>
                             <li className="hover:underline"><a href="">Whatsapp</a></li>
